e7e9b5c2647dd61302ffa8c0a37f320af575ffbd
DragonFang <myworkdragon@163.com>
2023-07-16 14:30:57 +0800

    调整 build_image.sh, 使支持build debug 镜像

--- docker/build_image.sh
+++ docker/build_image.sh
@@ -8,13 +8,27 @@ tag=${tag_suffix}
 #tag="release_20220311"
 image="workdragonfang/spider_proxy":${tag}
 
-echo "docker build"
-# docker build -t ${image} -f docker/Dockerfile .
-build_image ${image} docker/Dockerfile .
-# echo "DockerUname" $DockerUname
-# echo "DockerUpwd" $DockerUpwd
-# docker login -u $DockerUname -p $DockerUpwd
- 
+
+# 通过 tr 切分 并获取列表最后一个元素
+array=(`echo $image | tr '_' ' '` ) 
+last_element_index=$((${#array[*]}-1))
+last_element=(${array[$last_element_index]})
+echo 'last_element:' $last_element
+
+# 最后是 debug, 则build debug 镜像
+if [ $last_element == 'debug' ]
+then
+    # build 镜像
+    echo "docker build debug"
+    build_image ${image} docker/Dockerfile_debug .
+else
+    
+    # build 镜像
+    echo "docker build"
+    build_image ${image} docker/Dockerfile .
+fi
+
+
 docker login
 
 docker push ${image} 
