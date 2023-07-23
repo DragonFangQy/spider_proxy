62da38803e3da23ccc5d4443721cedb9ca62ff4e
DragonFang <myworkdragon@163.com>
2023-07-23 16:29:21 +0800

    temp

--- spider_proxy/utils/utils_log.py
+++ spider_proxy/utils/utils_log.py
@@ -5,7 +5,9 @@ import inspect
 
 import colorlog
 
+
 from logging.handlers import TimedRotatingFileHandler
+# from spider_proxy.utils.demo import TimedRotatingFileHandlerSafe as TimedRotatingFileHandler
 
 
 base_file_path = os.path.dirname( os.path.dirname(__file__) )
@@ -15,6 +17,8 @@ print(LOG_PATH)
 
 # level info 20 debug 10
 log_level = os.getenv("LOG_LEVEL", logging.INFO)
+# 默认debug 只log debug
+log_debug_log_all = os.getenv("LOG_DEBUG_LOG_ALL", "False")
 ### 日志 ###
 # 定义不同日志等级颜色
 log_colors_config = {
@@ -43,43 +47,37 @@ LOG_FORMATTER = colorlog.ColoredFormatter(
             style='%'
         ) 
 
-def make_log_folder(log_file_path):
-    save_dir = "/".join(log_file_path.split("/")[:-1])
-    if not os.path.exists(save_dir):
-        os.makedirs(save_dir)
-    return log_file_path
-
 # 控制日志输出路径，
 # 必须包含 logging.INFO
 LOG_INFO = {
     logging.DEBUG: {
-            "log_file_path": make_log_folder(LOG_PATH+"/debug/service.log"),
-            "log_handler": TimedRotatingFileHandler(
-                                filename=LOG_PATH+"/debug/service.log", 
-                                when="MIDNIGHT", 
-                                interval=1,
-                                backupCount=30
-                                ),
+            "log_file_path": LOG_PATH+"/debug/service.log",
+            # "log_handler": TimedRotatingFileHandler(
+            #                     filename=LOG_PATH+"/debug/service.log", 
+            #                     when="MIDNIGHT", 
+            #                     interval=1,
+            #                     backupCount=30
+            #                     ),
             "log_formatter": LOG_FORMATTER,
         },
     logging.INFO: {
-            "log_file_path": make_log_folder(LOG_PATH+"/info/service.log"),
-            "log_handler": TimedRotatingFileHandler(
-                                filename=LOG_PATH+"/info/service.log", 
-                                when="MIDNIGHT", 
-                                interval=1,
-                                backupCount=30
-                                ),
+            "log_file_path": LOG_PATH+"/info/service.log",
+            # "log_handler": TimedRotatingFileHandler(
+            #                     filename=LOG_PATH+"/info/service.log", 
+            #                     when="MIDNIGHT", 
+            #                     interval=1,
+            #                     backupCount=30
+            #                     ),
             "log_formatter": LOG_FORMATTER,
         },
     logging.ERROR: {
-            "log_file_path": make_log_folder(LOG_PATH+"/error/service.log"),
-            "log_handler": TimedRotatingFileHandler(
-                                filename=LOG_PATH+"/error/service.log", 
-                                when="MIDNIGHT", 
-                                interval=1,
-                                backupCount=30
-                                ),
+            "log_file_path": LOG_PATH+"/error/service.log",
+            # "log_handler": TimedRotatingFileHandler(
+            #                     filename=LOG_PATH+"/error/service.log", 
+            #                     when="MIDNIGHT", 
+            #                     interval=1,
+            #                     backupCount=30
+            #                     ),
             "log_formatter": LOG_FORMATTER,
         },
 }
@@ -91,15 +89,13 @@ class Logger(logging.Logger):
         self.print_console = print_console
         self.encoding = encoding
         self.level = level
-        self.__logger = logging.getLogger()
+        self.__logger = logging.getLogger(name)
         self.__logger.setLevel(self.level)
 
         self.EXCEPTION = 45
         
         # 创建日志目录
         self._make_log_folder()
-        # 
-        self._set_log_handler()
 
     @staticmethod
     def _make_log_folder():
@@ -113,10 +109,25 @@ class Logger(logging.Logger):
             if not os.path.exists(save_dir):
                 os.makedirs(save_dir)
     
-    def _set_log_handler(self):
+    def _set_log_handler(self, level):
         
+        handler_list = []
         for log_level, log_info in LOG_INFO.items():
-            log_handler = log_info.get("log_handler")
+
+            if level == self.EXCEPTION:
+                level = logging.ERROR
+
+            if level != log_level:
+                continue
+
+            log_file_path = log_info.get("log_file_path")
+            log_handler = TimedRotatingFileHandler(
+                                filename=log_file_path, 
+                                when="MIDNIGHT", 
+                                interval=1,
+                                backupCount=30
+                                )
+            
             log_formatter = log_info.get("log_formatter")
 
             if not isinstance(log_handler, logging.Handler):
@@ -124,24 +135,60 @@ class Logger(logging.Logger):
 
             log_handler.setFormatter(log_formatter) 
             log_handler.setLevel(level=log_level)
+            handler_list.append(log_handler)
             self.__logger.addHandler(log_handler)
         
-        if self.print_console:
-            log_formatter = LOG_INFO.get(logging.INFO).get("log_formatter")
-            console_handler = colorlog.StreamHandler()
-            console_handler.setFormatter(log_formatter) 
-            console_handler.setLevel(level=logging.INFO)
-            self.__logger.addHandler(console_handler)
+            # or (level != logging.DEBUG, logging.INFO) \
+            # or level > logging.INFO:
 
-    @staticmethod
-    def _close_handler():
-
-        for log_info in LOG_INFO.values():
-            log_handler = log_info.get("log_handler") 
+        if len(handler_list) == 0 or level > logging.INFO or log_debug_log_all.lower() == "True".lower():
+            # 必须存在 info
+            log_info = LOG_INFO.get(logging.INFO)
+            log_file_path = log_info.get("log_file_path")
+            log_handler = TimedRotatingFileHandler(
+                                filename=log_file_path, 
+                                when="MIDNIGHT", 
+                                interval=1,
+                                backupCount=30
+                                )
+            
+            log_formatter = log_info.get("log_formatter")
 
             if not isinstance(log_handler, logging.Handler):
                 raise ValueError(f"{log_handler} not isinstance logging.Handler ")
+
+            log_handler.setFormatter(log_formatter) 
+            log_handler.setLevel(level=logging.INFO)
+            handler_list.append(log_handler)
+            self.__logger.addHandler(log_handler)
+        elif level == logging.DEBUG:
+            pass
+
+        if self.print_console:
+            console_handler = colorlog.StreamHandler()
+
+            log_formatter = LOG_INFO.get(logging.INFO).get("log_formatter")
+            console_handler.setFormatter(log_formatter) 
+            console_handler.setLevel(level=logging.INFO)
+
+            handler_list.append(console_handler)
+            self.__logger.addHandler(console_handler)
+
+        return handler_list
+
+    def _close_handler(self, handler_list):
+
+        # for log_info in LOG_INFO.values():
+            # log_handler = log_info.get("log_handler") 
+
+            # if not isinstance(log_handler, logging.Handler):
+            #     raise ValueError(f"{log_handler} not isinstance logging.Handler ")
+        for log_handler in handler_list:
+            self.__logger.removeHandler(log_handler)
+            if not isinstance(log_handler, logging.Handler):
+                raise ValueError(f"{log_handler} not isinstance logging.Handler ")
             log_handler.close() 
+        print()
 
     def debug(self, message):
         self.__console(logging.DEBUG, message)
@@ -163,6 +210,9 @@ class Logger(logging.Logger):
 
     def __console(self, level, message):
             
+            # 
+            handler_list = self._set_log_handler(level)
+
             level_dict = {
                 logging.DEBUG:self.__logger.debug,
                 logging.INFO:self.__logger.info,
@@ -171,6 +221,7 @@ class Logger(logging.Logger):
                 self.EXCEPTION:self.__logger.exception,
                 logging.CRITICAL:self.__logger.critical,
             }
+
             # inspect
             frame = inspect.currentframe()
             co_filename, f_lineno, f_name = frame.f_back.f_back.f_code.co_filename, frame.f_back.f_back.f_lineno, frame.f_back.f_back.f_code.co_name
@@ -180,9 +231,9 @@ class Logger(logging.Logger):
             log_func = level_dict.get(level) 
             log_func(_message) 
 
-            self._close_handler()
+            self._close_handler(handler_list)
 
 
 log_name = "log_name.log"
 
-my_logger = Logger(name=log_name, level=int(log_level))
+my_logger = Logger(name=log_name, level=int(10))
