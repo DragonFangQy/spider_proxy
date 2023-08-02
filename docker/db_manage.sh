775bfe82a02f044299fa5bf9ef089c8e70ba3530
DragonFang <myworkdragon@163.com>
2023-08-03 07:45:42 +0800

    尝试使用 alembic 代替 sqlalchemy-migrate

--- docker/db_manage.sh
+++ docker/db_manage.sh
@@ -1,10 +1,12 @@
 
 
-cd /spider_proxy/db_repo 
+# cd /spider_proxy/db_repo 
 
-python manage.py version_control
+# python manage.py version_control
 
-python manage.py upgrade
+# python manage.py upgrade
 
+# cd /spider_proxy
 
-cd /spider_proxy
+# 尝试使用 alembic 代替 sqlalchemy-migrate
+alembic upgrade head
